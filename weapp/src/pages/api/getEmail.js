e7a03e50f29cef8980b686c08333d3cfe20bf5e3
Bash <contact.qmicon@gmail.com>
2023-03-01 00:16:39 +0530

    getemail import error fixed

--- weapp/src/pages/api/getEmail.js
+++ weapp/src/pages/api/getEmail.js
@@ -1,5 +1,5 @@
-import EmailSchema from "../../models/UsersFinal";
-import connectDB from "../../middleware/mongoose";
+import EmailSchema from "../../../models/UsersFinal";
+import connectDB from "../../../middleware/mongoose";
 
 const handler = async (req, res) => {
   let emails = await EmailSchema.find({email: req.query.email})
