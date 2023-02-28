59ed2f085c276b4073bc39a180e4697817ae9024
Shashwat Shukla <shashwat.shukla@juspay.in>
2023-02-28 16:32:40 +0530

    added context data and chart

---
+++ weapp/components/Chart/ChartContainer.js
@@ -0,0 +1,68 @@
+import Plotly from "plotly.js-dist-min";
+import React, { useEffect } from "react";
+import Web3 from "web3";
+import { useData } from "../../contexts/DataContext";
+
+
+const ChartContainer = ({ questionId }) => {
+  const { futureFlows } = useData();
+
+  const fetchGraphData = async () => {
+    var data = await futureFlows.methods.getGraphData(questionId).call();
+    var yesData = {
+      time: [],
+      amount: [],
+    };
+    var noData = {
+      time: [],
+      amount: [],
+    };
+    data["0"].forEach((item) => {
+      var sum = yesData.amount.reduce((a, b) => a + b, 0);
+      yesData.amount.push(
+        sum + parseFloat(Web3.utils.fromWei(item[1], ""))
+      );
+      yesData.time.push(new Date(parseInt(item[2] + "000")));
+    });
+    data["1"].forEach((item) => {
+      var sum = noData.amount.reduce((a, b) => a + b, 0);
+      noData.amount.push(
+        sum + parseFloat(Web3.utils.fromWei(item[1], ""))
+      );
+      noData.time.push(new Date(parseInt(item[2] + "000")));
+    });
+
+    var yes = {
+      x: [...yesData.time],
+      y: [...yesData.amount],
+      mode: "lines+markers",
+      name: "Yes",
+    };
+
+    var no = {
+      x: [...noData.time],
+      y: [...noData.amount],
+      mode: "lines+markers",
+      name: "No",
+    };
+    var chartData = [yes, no];
+
+    var layout = {
+      title: "YES / NO Graph",
+    };
+
+    Plotly.newPlot("myDiv", chartData, layout, { displayModeBar: false });
+  };
+
+  useEffect(() => {
+    fetchGraphData();
+  });
+
+  return (
+    <>
+      <div id="myDiv"></div>
+    </>
+  );
+};
+
+export default ChartContainer;
---
+++ weapp/contexts/DataContext.js
@@ -0,0 +1,78 @@
+import { createContext, useContext, useState } from "react";
+import Web3 from "web3";
+import FutureFlowsMarket from "../abis/FutureFlowsMarket.json";
+import FutureFlowsToken from "../abis/FutureFlowsToken.json";
+
+
+
+const DataContext = createContext({
+  account: "",
+  loading: true,
+  loadWeb3: async () => {},
+  futureFlowsMarket: null,
+  futureFlowsToken: null,
+});
+
+export const DataProvider = ({ children }) => {
+  const data = useProviderData();
+
+  return <DataContext.Provider value={data}>{children}</DataContext.Provider>;
+};
+
+export const useData = () => useContext(DataContext);
+
+export const useProviderData = () => {
+  const [loading, setLoading] = useState(true);
+  const [account, setAccount] = useState("");
+  const [futureFlowsMarket, setFutureFlowsMarket] = useState();
+  const [futureFlowsToken, setFutureFlowsToken] = useState();
+
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert("Non-Eth browser detected. Please consider using MetaMask.");
+      return;
+    }
+    var allAccounts = await window.web3.eth.getAccounts();
+    setAccount(allAccounts[0]);
+    await loadBlockchainData();
+  };
+
+  const loadBlockchainData = async () => {
+    const web3 = window.web3;
+
+    const futureFlowMarketData = FutureFlowsMarket.networks["80001"];
+    const futureFlowTokenData = FutureFlowsToken.networks["80001"];
+
+    if (futureFlowMarketData && futureFlowTokenData) {
+      var tempContract = await new web3.eth.Contract(
+        FutureFlowsMarket.abi,
+        futureFlowMarketData.address
+      );
+      setFutureFlowsMarket(tempContract);
+      var tempTokenContract = await new web3.eth.Contract(
+        FutureFlowsToken.abi,
+        futureFlowTokenData.address
+      );
+
+      setFutureFlowsToken(tempTokenContract);
+    } else {
+      window.alert("TestNet not found");
+    }
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  };
+
+  return {
+    account,
+    futureFlowsMarket,
+    futureFlowsToken,
+    loading,
+    loadWeb3,
+  };
+};
